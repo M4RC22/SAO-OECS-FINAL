dad1d39ccb474b84b105d52b37894e6e9e9d37f7
Paul Signo <pdsigno@student.apc.edu.ph>
2022-11-04 16:52:28 +0800

    Fix error in APFController

--- app/Http/Controllers/RFController.php
+++ app/Http/Controllers/RFController.php
@@ -43,7 +43,7 @@ class RFController extends Controller
 
          // get ID for approvers
         $orgAdviser = OrganizationUser::where('organization_id',$event->organization_id)
-         ->where('position', 'Adviser')->pluck('id')->first();
+         ->where('position', 'Adviser')->first();
          
         //Check first if student organization have an adviser before continuing the process, else return error. 
         if($orgAdviser === null){
@@ -69,7 +69,7 @@ class RFController extends Controller
             'organization_id' => $event->organization_id,
             'prep_by' => Auth::user()->id,
             'control_number'=> $this->generateUniqueCode(),
-            'organization_user_adviser_id' => $orgAdviser,
+            'organization_user_adviser_id' => $orgAdviser->id,
             'sao_staff_id' => $sao,
             'acadserv_staff_id' => $acadserv ,
             'finance_staff_id' => $finance,
